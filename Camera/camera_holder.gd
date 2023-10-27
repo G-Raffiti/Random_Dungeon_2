348073e3493c5a1cde317271f55675676e0a5224
Raphael Bonneval <grugroo128@gmail.com>
2023-10-27 02:02:25 +0200

    ADD Camera movement

---
+++ Camera/camera_holder.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+@export var player: Node2D
+@export var speed: float
+var follow: bool = false
+
+func _ready():
+    if player != null:
+        player.tree_exiting.connect(_on_player_tree_exiting)
+        follow = true
+
+func _on_player_tree_exiting():
+    follow = false
+
+func _physics_process(delta):
+    if not follow: return
+    var destination = (get_global_mouse_position() - player.global_position) / 4 + player.global_position
+    var distance = (destination - global_position).length()
+    global_position = global_position.move_toward(destination, delta * distance * distance * speed)
+
+func _exit_tree():
+    if player != null:
+        player.tree_exiting.disconnect(_on_player_tree_exiting)
